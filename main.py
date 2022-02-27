41880265af2554db1ee42e9b3a3fa9c4e1119da9
panda7802 <814252020@qq.com>
2022-02-27 17:10:47 +0800

    增加时间

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Alt+Shift+X to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import time
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +13,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    tag = f'推荐-{str(time.strftime("%Y-%m-%d %H:%M:%S"))}'
+    print(tag)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
